551c7921bdda6f82323d3b29f29bb5a835812362
SavonnaB <143576792+SavonnaB@users.noreply.github.com>
2025-06-25 20:44:29 +0000

    added method to return size

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -5,12 +5,14 @@ public class DoubleLinkedList implements Comparable<nfs_DoubleLinkedList> {
     private String name;
     private Node head;
     private Node tail;
+    private int size; //SB: added size declaration
 
     /** Basic constructor */
     public DoubleLinkedList(String name) {
         this.name = name;
         this.head = null;
         this.tail = null;
+        this.size = 0; //SB: size initialized to 0
     } // Basic constructor
 
     /** Default constructor passes default name to basic constructor */
@@ -35,7 +37,13 @@ public class DoubleLinkedList implements Comparable<nfs_DoubleLinkedList> {
             node.setPrev(this.tail); //SB: sets previous item in doublelinked list
             this.tail = node;
         }
+        this.size++; //SB: this should increment the size
     } // method add
+
+    //SB: Method to return size
+    public int size() {
+    return this.size;
+    } //SB: Not sure about the placement here but it is inside the class. Also not sure if legal due to having more than one return statement in this method.
 
     // overload method add to add a node by value
     public void add(String value) {
